6f2e048e1bfeacccad65103382b991f1e8471099
prashant <prashantkhobragade011@gmail.com>
2023-10-12 20:21:14 +0530

    app py completed

--- app.py
+++ app.py
@@ -14,24 +14,21 @@ CORS(app)
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
-        self.classifier = PredictionPipeline(self.filename)
+        self.classifire = PredictionPipeline(self.filename)
         
 
 @app.route("/", methods=['GET'])
-@cross_origin()
 def home():
     return render_template('index.html')
 
 
 @app.route("/train", methods = ['GET','POST'])
-@cross_origin
 def trainRoute():
     os.system("python main.py")
     #os.system("dvc repro")
     return "Training done successfully !!"
 
 @app.route("/predict", methods = ['POST'])
-@cross_origin
 def predictRoute():
     image = request.json['image']
     decodeImage(image, clApp.filename)
